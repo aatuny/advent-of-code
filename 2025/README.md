7eeefabea52c79d8e487bcac41a5a6c929f8ffaf
Aatu Nykänen <27770287+aatuny@users.noreply.github.com>
2025-12-01 20:17:11 +0200

    Improve readability to match level of solutions available in Internet

--- 2025/README.md
+++ 2025/README.md
@@ -6,6 +6,5 @@ Few points regarding the puzzle solutions posted to this repository:
 
 - Each day is written as lib module which is usable through cli executable (bin). Cli takes exactly one argument: number for the day to acquire solution (with addition of "warmup").
 - Each day reads input from within personal-input -folder using file naming convention of `yyyy-mm-dd.txt`. All inputs are omitted from repo per AoC FAQ.
-- The code readability will probably be awful(ish) and some of it will be on purpose to avoid including any part of the puzzle text to code/comments in order to respect the AoC FAQ guidelines.
 - Tests are not implemented (again with purpose of avoiding to include any inputs).
 - Input format is trusted implicitly. If input file is broken, the solution will also produce panic.
--- 2025/src/aoc/day_1.rs
+++ 2025/src/aoc/day_1.rs
@@ -7,20 +7,20 @@ pub fn solve_day() {
 }
 
 pub fn solve_part_1(data: &str) {
-    let mut v = 50;
+    let mut dial = 50;
     let mut result = 0;
 
     for line in data.lines() {
-        let d = line.chars().nth(0).unwrap();
-        let a = line[1..].parse::<i32>().unwrap() % 100;
+        let directive = line.chars().nth(0).unwrap();
+        let move_dial = line[1..].parse::<i32>().unwrap() % 100;
 
-        v = match d {
-            'L' => calculate_result(v - a),
-            'R' => calculate_result(v + a),
-            _ => panic!("Cannot calculate {d}"),
+        dial = match directive {
+            'L' => calculate_dial(dial - move_dial),
+            'R' => calculate_dial(dial + move_dial),
+            _ => panic!("Cannot calculate {directive}"),
         };
 
-        if v == 0 {
+        if dial == 0 {
             result += 1;
         }
     }
@@ -29,60 +29,58 @@ pub fn solve_part_1(data: &str) {
 }
 
 pub fn solve_part_2(data: &str) {
-    let mut v = 50;
+    let mut dial = 50;
     let mut result = 0;
 
     for line in data.lines() {
-        let d = line.chars().nth(0).unwrap();
-        let a = line[1..].parse::<i32>().unwrap();
+        let directive = line.chars().nth(0).unwrap();
+        let amount = line[1..].parse::<i32>().unwrap();
 
-        let an_1 = a % 100;
-        let an_2: i32 = a / 100;
+        let move_dial = amount % 100;
+        let full_rounds: i32 = amount / 100;
 
-        v = match d {
+        dial = match directive {
             'L' => {
-                let local_result = v - an_1;
-                if local_result < 0 && v != 0 {
+                let local_result = dial - move_dial;
+                if local_result < 0 && dial != 0 {
                     result += 1;
                 }
 
-                calculate_result(v - an_1)
+                calculate_dial(local_result)
             }
             'R' => {
-                let local_result = v + an_1;
+                let local_result = dial + move_dial;
                 if local_result > 100 {
                     result += 1;
                 }
 
-                calculate_result(v + an_1)
+                calculate_dial(local_result)
             }
-            _ => panic!("Cannot calculate {d}"),
+            _ => panic!("Cannot calculate {directive}"),
         };
 
-        if v == 0 {
+        if dial == 0 {
             result += 1;
         }
 
-        if an_2 > 0 {
-            result += an_2;
-        }
+        result += full_rounds;
     }
 
     println!("Day 1 part 2 answer is {result}");
 }
 
-fn calculate_result(result: i32) -> i32 {
-    if result == 0 || result == 100 {
+fn calculate_dial(dial: i32) -> i32 {
+    if dial == 100 {
         return 0;
     }
 
-    if result > 100 {
-        return result - 100;
+    if dial > 100 {
+        return dial - 100;
     }
 
-    if result < 0 {
-        return 100 - result.abs();
+    if dial < 0 {
+        return dial + 100;
     }
 
-    result
+    dial
 }
