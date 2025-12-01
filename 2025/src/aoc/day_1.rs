cb8a721e60de15bef1afa36eb373baf62fd4cc59
Aatu Nykänen <27770287+aatuny@users.noreply.github.com>
2025-12-01 19:21:49 +0200

    Solve day 1 part 2

--- 2025/src/aoc/day_1.rs
+++ 2025/src/aoc/day_1.rs
@@ -1,13 +1,12 @@
 use crate::aoc_utils;
 
 pub fn solve_day() {
-    solve_part_1();
-    solve_part_2();
+    let data = aoc_utils::read_file_mem("personal-input/2025-12-01.txt");
+    solve_part_1(data.as_str());
+    solve_part_2(data.as_str());
 }
 
-pub fn solve_part_1() {
-    let data = aoc_utils::read_file_mem("personal-input/2025-12-01.txt");
-
+pub fn solve_part_1(data: &str) {
     let mut v = 50;
     let mut result = 0;
 
@@ -29,8 +28,47 @@ pub fn solve_part_1() {
     println!("Day 1 part 1 answer is {result}");
 }
 
-pub fn solve_part_2() {
-    println!("Day 1 part 2 has not yet been solved.")
+pub fn solve_part_2(data: &str) {
+    let mut v = 50;
+    let mut result = 0;
+
+    for line in data.lines() {
+        let d = line.chars().nth(0).unwrap();
+        let a = line[1..].parse::<i32>().unwrap();
+
+        let an_1 = a % 100;
+        let an_2: i32 = a / 100;
+
+        v = match d {
+            'L' => {
+                let local_result = v - an_1;
+                if local_result < 0 && v != 0 {
+                    result += 1;
+                }
+
+                calculate_result(v - an_1)
+            }
+            'R' => {
+                let local_result = v + an_1;
+                if local_result > 100 {
+                    result += 1;
+                }
+
+                calculate_result(v + an_1)
+            }
+            _ => panic!("Cannot calculate {d}"),
+        };
+
+        if v == 0 {
+            result += 1;
+        }
+
+        if an_2 > 0 {
+            result += an_2;
+        }
+    }
+
+    println!("Day 1 part 2 answer is {result}");
 }
 
 fn calculate_result(result: i32) -> i32 {
